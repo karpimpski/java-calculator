a628d4b660de94fcc91d2fbf06c589007b68ddb0
Michael Karpinski <mike.karpinski99@gmail.com>
2020-08-15 13:38:48 -0400

    basic calculator with mouse functionality

---
+++ src/main/Controller.java
@@ -0,0 +1,80 @@
+package main;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
+public class Controller
+{
+    String[] numbers = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    String[] operands = {"%", "/", "*", "-", "+"};
+    String[] special = {"(", ")", "C", "="};
+    String decimal = ".";
+
+    boolean resultDisplayed = false;
+
+    public void addCharacter(TextField textField, String character)
+    {
+        if (resultDisplayed)
+        {
+            textField.setText("");
+            resultDisplayed = false;
+        }
+        textField.setText(textField.getText() + character);
+    }
+
+    public void clearText(TextField textField)
+    {
+        textField.setText("");
+        resultDisplayed = false;
+    }
+
+    public void click(MouseEvent event)
+    {
+        Button button = (Button) event.getSource();
+        String text = button.getText();
+        handleInput(button.getText(), button.getScene());
+    }
+
+    public boolean contains(String[] list, String text)
+    {
+        for (int i = 0; i < list.length; i++)
+        {
+            if (list[i].equals(text))
+                return true;
+        }
+        return false;
+    }
+
+    public void handleInput(String text, Scene scene)
+    {
+        TextField textField = (TextField) scene.lookup("#textField");
+        String fieldText = textField.getText();
+
+        String lastCharacter = "";
+        if (fieldText.length() > 0)
+            lastCharacter = textField.getText().substring(textField.getText().length() - 1);
+
+        if (contains(numbers, text))
+            addCharacter(textField, text);
+        if (contains(operands, text) && !contains(operands, lastCharacter) && textField.getText().length() > 0)
+            addCharacter(textField, text);
+        if (text.equals(decimal) && !decimal.equals(lastCharacter))
+            addCharacter(textField, text);
+        if (contains(special, text))
+        {
+            if (text.equals("C"))
+                clearText(textField);
+            else if (text.equals("="))
+            {
+                double result = new ExpressionBuilder(textField.getText()).build().evaluate();
+                textField.setText(String.valueOf(result));
+                resultDisplayed = true;
+            }
+            else
+                addCharacter(textField, text);
+        }
+    }
+}
